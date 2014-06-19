03948d48b8d27927d11fd9a4dfaee7dd0de3676c
Administrator <administrator@vpn243.local>
2014-06-19 12:55:29 +0200

    rdfs is now generated

--- prototype/static/js/annotator.neonion.js
+++ prototype/static/js/annotator.neonion.js
@@ -10,22 +10,32 @@ Annotator.Plugin.Neonion = function (element, options) {
 				}
 			})
 
-// neues Feld im Editor erzeugen
-/*
+			// neues Feld im Editor erzeugen
+			/*
 			this.annotator.editor.addField({
 				label: 'Name',
 				type: 'input',
 				load: this.updateField,
 				submit: this.setAnnotationTags
 			});
-*/
+			*/
 
 			this.annotator.subscribe("annotationCreated", function (annotation) {
-				//alert("edit");
-				// RDFa zu annotation Objekt hinzufügen
-				// Markup updaten
-			});
+				// update annotation object
+				annotation.rdf = { 
+					typeof : "dbpedia:Country",
+					property : "rdfs:label",
+					about : "http://www.loomp.org/dic/pi/0.1/PFXEMONGDGT4ZP7U8NWKJ0ZAR05CN298"
+				 };
+				// add RDFa attributes to markup
+				annotation.highlights[0].setAttribute("typeof", annotation.rdf.typeof);
+				annotation.highlights[0].setAttribute("property", annotation.rdf.property);
+				annotation.highlights[0].setAttribute("about", annotation.rdf.about);
+				
+				//console.log(annotation.highlights[0]);	
 
+			});
+			console.log(this.annotator);	
 		}
 	}
 

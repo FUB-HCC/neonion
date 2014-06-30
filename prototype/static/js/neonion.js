492c223aafdf939bad2e864a1e60627dd93db985
andre.breitenfeld <administrator@vpn243.local>
2014-06-30 17:00:19 +0200

    save document in loomp

---
+++ prototype/static/js/neonion.js
@@ -0,0 +1,15 @@
+var config = {
+    server : "http://localhost:8181/loomp",
+
+    uri : {
+        annotation : "http://vocab.loomp.org/model/Annotation",
+        annotationSet : "http://vocab.loomp.org/model/AnnotationSet",
+        elementText : "http://vocab.loomp.org/model/ElementText"
+    },
+    
+    service : {
+        get : "/content/get",
+        getAll : "/content/getAll",
+        save : "/content/save"
+    }
+}; 

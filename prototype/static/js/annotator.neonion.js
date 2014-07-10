2c55fc1ecfb4a3d1502b77982d4d085e8039e9fe
andre.breitenfeld <administrator@vpn243.local>
2014-07-10 17:23:12 +0200

    added preselection of person to annotator plugin

--- prototype/static/js/annotator.neonion.js
+++ prototype/static/js/annotator.neonion.js
@@ -1,5 +1,13 @@
 Annotator.Plugin.Neonion = function (element, options) {
     
+	var persons = [ 
+		{ label: "Unbekannte Person", uri : "http://de.dbpedia.org/resource/Unknown_Person" },
+		{ label: "Otto Hahn", uri : "http://de.dbpedia.org/resource/Otto_Hahn" }, 
+		{ label: "Max Planck", uri : "http://de.dbpedia.org/resource/Max_Planck" },  
+		{ label: "Otto von Baeyer", uri : "http://de.dbpedia.org/resource/Otto_von_Baeyer" },  
+		{ label: "Wilhelm Westphal", uri : "http://de.dbpedia.org/resource/Wilhelm_Westphal_(Physiker)" } 
+	];
+
     return {
 
 		pluginInit : function () {
@@ -8,44 +16,69 @@ Annotator.Plugin.Neonion = function (element, options) {
 
 			this.annotator.viewer.addField({
 				load: function (field, annotation) {
-					field.innerHTML = "Person";
+					field.innerHTML = "Person: <a href='" + annotation.rdf.about + "' target='blank'>" + annotation.rdf.label + "</a>";
 				}
 			})
 
-			// neues Feld im Editor erzeugen
-			/*
-			this.annotator.editor.addField({
-				label: 'Name',
-				type: 'input',
-				load: this.updateField,
-				submit: this.setAnnotationTags
+			var suggestionField = this.annotator.editor.addField({
+				load: this.updateSuggestionField,
+				submit: this.pluginSubmit
 			});
-			*/
+			$(suggestionField).children((":first")).replaceWith("<div class='btn-group-vertical suggestionItems'></div>");
+			$(suggestionField).click(function (e) {
+				var source = $(e.target);
 
+				source.parent().children().removeClass("active");
+				$(source).addClass("active");
+				$(".annotator-widget").submit();
+			});
+	
+			/*
 			this.annotator.subscribe("beforeAnnotationCreated", function (annotation) {
 				
 			});
+			*/
 
-			this.annotator.subscribe("annotationCreated", function (annotation) {
-				// update annotation object
-				annotation.rdf = { 
-					typeof : "dbpedia:Country",
-					property : "rdfs:label",
-					about : "http://www.loomp.org/dic/pi/0.1/PFXEMONGDGT4ZP7U8NWKJ0ZAR05CN298"
-				 };
+			this.annotator.subscribe("annotationCreated", this.enrichRDFa);
+			this.annotator.subscribe("annotationUpdated", this.enrichRDFa);
 
-				// add RDFa attributes to markup
-				annotation.highlights[0].setAttribute("typeof", annotation.rdf.typeof);
-				annotation.highlights[0].setAttribute("property", annotation.rdf.property);
-				annotation.highlights[0].setAttribute("about", annotation.rdf.about);
-				
-				//console.log(annotation.highlights[0]);	
-				//console.log(annotation);	
-				
+		},
+		
+		enrichRDFa : function(annotation) {
+			// add RDFa attributes to markup
+			annotation.highlights[0].setAttribute("typeof", annotation.rdf.typeof);
+			annotation.highlights[0].setAttribute("property", annotation.rdf.property);
+			annotation.highlights[0].setAttribute("about", annotation.rdf.about);
+		},
 
+		pluginSubmit : function(field, annotation) {
+			var activeItem = $(field).children(":first").children(".active");
+
+			// update annotation object
+			annotation.rdf = { 
+				typeof : "dbpedia-owl:Person",
+				property : "rdfs:label",
+				about : activeItem.attr("uri"), 
+				label : activeItem.text()
+			 };
+		},
+
+		updateSuggestionField : function(field, annotation) {
+			var list = $(field).children(":first");
+			var associatedUri = annotation.rdf ? annotation.rdf.about : "";
+
+			list.empty();
+			// add items
+			$.each(persons, function(index, value) {
+				var css = index > 0 ? "btn-default" : "btn-primary";
+
+				list.append("<button type='button' class='btn " + css + "' uri='" +  value.uri + "'>" + value.label + "</button>");
+				if (associatedUri == value.uri) {
+					list.children(":last").addClass("active");
+				}
 			});
-			
 		}
+
 	}
 
 };

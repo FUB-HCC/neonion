93f9af7150a2debe3231326688656141fe0190fd
Lukas Benedix <benedix@zedat.fu-berlin.de>
2014-07-17 21:08:17 +0200

    fixed ugly url problem

--- prototype/static/js/neonion.js
+++ prototype/static/js/neonion.js
@@ -1,16 +1,18 @@
 var config = {
-    
+
     server : "http://hcc-loomp.herokuapp.com",
+
+    uriPrefix: "http://loomp.org/data/",
 
     uri : {
         annotation : "http://vocab.loomp.org/model/Annotation",
         annotationSet : "http://vocab.loomp.org/model/AnnotationSet",
         elementText : "http://vocab.loomp.org/model/ElementText"
     },
-    
+
     service : {
         get : "/content/get",
         getAll : "/content/getAll",
         save : "/content/save"
     }
-}; 
+};

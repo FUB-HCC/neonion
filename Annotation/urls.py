35e840d793bdafd8b949efae99423da83927ea8c
Lukas Benedix <benedix@zedat.fu-berlin.de>
2014-07-19 23:35:26 +0200

    added simple user management

--- Annotation/urls.py
+++ Annotation/urls.py
@@ -6,7 +6,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'prototype.views.home', name='home'),
     url(r'^annotator/$', 'prototype.views.home', name='home'),
-    
+
     url(r'^annotator/(?P<doc_id>.+)/$', 'prototype.views.annotator', name='annotator'),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='auth_logout'),
+    url(r'^logout/(?P<next_page>.*)/$', 'django.contrib.auth.views.logout', name='auth_logout_next'),
+
+
 )
--- prototype/views.py
+++ prototype/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def home(request):
     return render_to_response('base_overview.html', {}, context_instance=RequestContext(request))
 
+@login_required
 def annotator(request, doc_id):
     return render_to_response('base_annotator.html', { "doc_id" : doc_id }, context_instance=RequestContext(request))

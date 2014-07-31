3af80b0ff65ab13e2bbf946de616fbab94675616
andre.breitenfeld <administrator@vpn243.local>
2014-07-31 11:58:47 +0200

    added base page for import document

--- Annotation/urls.py
+++ Annotation/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'prototype.views.home', name='home'),
     url(r'^annotator/$', 'prototype.views.home', name='home'),
+	url(r'^import/$', 'prototype.views.import_document', name='annotator'),
 
     url(r'^annotator/(?P<doc_id>.+)/$', 'prototype.views.annotator', name='annotator'),
     url(r'^admin/', include(admin.site.urls)),
--- prototype/views.py
+++ prototype/views.py
@@ -11,3 +11,7 @@ def home(request):
 @login_required
 def annotator(request, doc_id):
     return render_to_response('base_annotator.html', { "doc_id" : doc_id }, context_instance=RequestContext(request))
+
+@login_required
+def import_document(request):
+    return render_to_response('base_import.html', { }, context_instance=RequestContext(request))

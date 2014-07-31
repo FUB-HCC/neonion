70bf5b879685a766a7712d2bd4c632fd14024859
Lukas Benedix <benedix@zedat.fu-berlin.de>
2014-07-31 12:44:22 +0200

    merge

--- prototype/views.py
+++ prototype/views.py
@@ -10,3 +10,8 @@ def home(request):
 
 # @login_required
 def annotator(request, doc_id):
+    return render_to_response('base_annotator.html', { "doc_id" : doc_id }, context_instance=RequestContext(request))
+
+# @login_required
+def import_document(request):
+    return render_to_response('base_import.html', { }, context_instance=RequestContext(request))

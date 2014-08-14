b766fe1e746352f90131e471f2e026895807e60b
andre.breitenfeld <administrator@vpn243.local>
2014-08-14 13:38:06 +0200

    connected WikiData search with Neonion plugin

--- prototype/static/js/annotator.neonion.js
+++ prototype/static/js/annotator.neonion.js
@@ -1,13 +1,5 @@
 Annotator.Plugin.Neonion = function (element, options) {
 
-	var persons = [
-		{ label: "Unbekannte Person", uri : "http://neonion.com/resource/Unknown_Person" },
-		{ label: "Otto Hahn", uri : "https://www.wikidata.org/wiki/Q57065" },
-		{ label: "Max Planck", uri : "https://www.wikidata.org/wiki/Q9021" },
-		{ label: "Otto von Baeyer", uri : "https://www.wikidata.org/wiki/Q1682101" },
-		{ label: "Wilhelm Westphal", uri : "https://www.wikidata.org/wiki/Q95679" }
-	];
-
 	var user;
 
     return {
@@ -17,17 +9,17 @@ Annotator.Plugin.Neonion = function (element, options) {
 			// add field to linked person
 			this.annotator.viewer.addField({
 				load: function (field, annotation) {
-					field.innerHTML = "Person: <a href='" + annotation.rdf.about + "' target='blank'>" + annotation.rdf.label + "</a>";
+					field.innerHTML = Annotator.Plugin.Neonion.prototype.literals['de'].person + ": <a href='" + annotation.rdf.about + "' target='blank'>" + annotation.rdf.label + "</a>";
 				}
 			});
 			// add field with creator
 			this.annotator.viewer.addField({
 				load: function (field, annotation) {
-					var userField = "Unbekannt";
+					var userField = Annotator.Plugin.Neonion.prototype.literals['de'].unknown;
 					if (annotation.creator) {
 						userField = annotation.creator.email;
 					}
-					field.innerHTML = "Erfasser: " + userField + "</a>";
+					field.innerHTML = Annotator.Plugin.Neonion.prototype['de'].literals.creator + ": " + userField + "</a>";
 				}
 			});
 
@@ -36,7 +28,7 @@ Annotator.Plugin.Neonion = function (element, options) {
 				load: this.updateSuggestionField,
 				submit: this.pluginSubmit
 			});
-			$(suggestionField).children((":first")).replaceWith("<div class='btn-group-vertical suggestionItems'></div>");
+			$(suggestionField).children((":first")).replaceWith("<div class='btn-group-vertical'></div>");
 			$(suggestionField).click(function (e) {
 				var source = $(e.target);
 
@@ -55,20 +47,12 @@ Annotator.Plugin.Neonion = function (element, options) {
 			});
 
 			// attach event handler to enrich DOM with RDFa
-			this.annotator.subscribe("annotationCreated", this.enrichRDFa);
-			this.annotator.subscribe("annotationUpdated", this.enrichRDFa);
-
+			this.annotator.subscribe("annotationCreated", Annotator.Plugin.Neonion.prototype.enrichRDFa);
+			this.annotator.subscribe("annotationUpdated", Annotator.Plugin.Neonion.prototype.enrichRDFa);
 		},
 
 		setUser : function(userData) { 
 			user = userData; 
-		},
-
-		enrichRDFa : function(annotation) {
-			// add RDFa attributes to markup
-			annotation.highlights[0].setAttribute("typeof", annotation.rdf.typeof);
-			annotation.highlights[0].setAttribute("property", annotation.rdf.property);
-			annotation.highlights[0].setAttribute("about", annotation.rdf.about);
 		},
 
 		pluginSubmit : function(field, annotation) {
@@ -79,7 +63,7 @@ Annotator.Plugin.Neonion = function (element, options) {
 
 			// update annotation object
 			annotation.rdf = {
-				typeof : "dbpedia-owl:Person", // "gndo:DifferentiatedPerson",
+				typeof : "dbpedia-owl:Person",
 				property : "rdfs:label",
 				about : activeItem.attr("uri"),
 				label : activeItem.text()
@@ -87,21 +71,76 @@ Annotator.Plugin.Neonion = function (element, options) {
 		},
 
 		updateSuggestionField : function(field, annotation) {
+			// get selected text
+			var words = annotation.quote.split(" ");
 			var list = $(field).children(":first");
 			var associatedUri = annotation.rdf ? annotation.rdf.about : "";
-
 			list.empty();
-			// add items
-			$.each(persons, function(index, value) {
-				// var css = index == 0 ? "btn-green" : "";
-				var css = "";
-				list.append("<button type='button' class='annotator-btn " + css + "' uri='" +  value.uri + "'>" + value.label + "</button>");
-				if (associatedUri == value.uri) {
-					list.children(":last").addClass("active");
-				}
+
+			options.wikiData.search_items(words[words.length - 1], function(wdID) {
+				options.wikiData.get_person_data(wdID.join('|'), function(persons) {
+					// sort list
+					persons.sort(Annotator.Plugin.Neonion.prototype.compareByLabel);
+					persons.unshift(Annotator.Plugin.Neonion.prototype.surrogate.unknownPerson);
+					// add items
+					$.each(persons, function(index, value) {
+						list.append(
+							"<button type='button' class='annotator-btn' uri='" + options.wikiData.prefix +  value.id + "'>" 
+							+ Annotator.Plugin.Neonion.prototype.formatters.personLabel(value)
+							+ "</button>"
+						);
+						if (associatedUri == value.id) {
+							list.children(":last").addClass("btn-active");
+						}
+					});
+				});
 			});
 		}
 
 	}
 
 };
+
+// Prototype extensions
+Annotator.Plugin.Neonion.prototype.literals = {
+	de : {
+		person : "Person", unknownPerson : "Unbekannte Person",
+		institute : "Institut", unknownInstitute : "Unbekanntes Institut",
+		creator : "Erfasser", unknown : "Unbekannt"
+	}
+}
+
+Annotator.Plugin.Neonion.prototype.surrogate = {
+	unknownPerson : { label : Annotator.Plugin.Neonion.prototype.literals['de'].unknownPerson, id : "http://neonion.com/resource/Unknown_Person" },
+	unknownInstitute : { label : Annotator.Plugin.Neonion.prototype.literals['de'].unknownInstitute, id : "http://neonion.com/resource/Unknown_Institute" }
+}
+
+Annotator.Plugin.Neonion.prototype.enrichRDFa = function(annotation) {
+	// add RDFa attributes to markup
+	annotation.highlights[0].setAttribute("typeof", annotation.rdf.typeof);
+	annotation.highlights[0].setAttribute("property", annotation.rdf.property);
+	annotation.highlights[0].setAttribute("about", annotation.rdf.about);
+}
+
+Annotator.Plugin.Neonion.prototype.compareByLabel = function(a, b) {
+	if (a.label < b.label)
+		return -1;
+	if (a.label > b.label)
+		return 1;
+	return 0;
+}
+
+Annotator.Plugin.Neonion.prototype.formatters = {
+
+	personLabel : function(value) {
+		var label = "<span>" + value.label + "</span>";
+		if (value.birth) label += "<small>&nbsp;" + value.birth + "</small>";
+		return label;
+	},
+
+	instituteLabel : function(value) {
+		var label = "<span>" + value.label + "</span>";
+		// TODO
+		return label;
+	}
+}

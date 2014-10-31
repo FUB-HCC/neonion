b35f8cce17196ffa58884fc698ee9ce2b605cbf0
andre.breitenfeld <administrator@vpn243.local>
2014-10-31 13:11:53 +0100

    changed endpoint url

--- settings/development.py
+++ settings/development.py
@@ -5,4 +5,4 @@ ANNOTATION_STORE_URL = "http://annotator.neonion.imp.fu-berlin.de"
 ELASTICSEARCH_URL = "http://localhost:9200"
 EULER_URL = 'http://euler.mpiwg-berlin.mpg.de:8000'
 ENDPOINT = 'http://localhost:8080/openrdf-sesame/repositories/neonion'
-ENDPOINT_UPDATE = 'http://localhost:8080/openrdf-workbench/repositories/neonion/update'
+ENDPOINT_UPDATE = 'http://localhost:8080/openrdf-sesame/repositories/neonion/statements'

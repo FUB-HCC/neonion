34808b786e5ef4ca96fbff01781797ce07877f0a
Lukas Benedix <benedix@zedat.fu-berlin.de>
2014-11-26 12:42:50 +0100

    small fixes

--- maintenance/config.py
+++ maintenance/config.py
@@ -1,4 +1,4 @@
 wd_url = 'https://dumps.wikimedia.org/other/wikidata/{}'
 
-dumps_folder = '/web/neonion.imp.fu-berlin.de/neonion/neonion/maintenance/dumps'
-extract_folder = '/web/neonion.imp.fu-berlin.de/neonion/neonion/maintenance/extracted_data'
+dumps_folder = '/web/neonion.imp.fu-berlin.de/neonion/maintenance/dumps'
+extract_folder = '/web/neonion.imp.fu-berlin.de/neonion/maintenance/extracted_data'
--- maintenance/maintainence.py
+++ maintenance/maintainence.py
@@ -3,7 +3,7 @@ import wd_download
 import wd_extract
 import wd_import
 
-# /web/neonion.imp.fu-berlin.de/neonion/neonion/maintenance/venv27/bin/python /web/neonion.imp.fu-berlin.de/neonion/neonion/maintenance/main.py
+# /web/neonion.imp.fu-berlin.de/neonion/neonion/maintenance/venv27/bin/python /web/neonion.imp.fu-berlin.de/neonion/neonion/maintenance/maintainence.py
 
 if __name__ == '__main__':
     wd_download.download_wd_dump(config.dumps_folder)

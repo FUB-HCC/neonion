86e7babcb08936bc4d1032e668739020ddc6a467
lbenedix <lukas.benedix@fu-berlin.de>
2014-12-18 16:14:04 +0100

    removed maintainence scripts (migrated to new Provider Class)
    Former-commit-id: e9dc27653e6658960f757b5ba1a6fb719ea48e99

--- maintenance/config.py
+++
@@ -1,5 +0,0 @@
-wd_url = 'https://dumps.wikimedia.org/other/wikidata/{}'
-
-root_folder = '/web/neonion.imp.fu-berlin.de/maintenance'
-dumps_folder = '/web/neonion.imp.fu-berlin.de/maintenance/dumps'
-extract_folder = '/web/neonion.imp.fu-berlin.de/maintenance/extracted_data'

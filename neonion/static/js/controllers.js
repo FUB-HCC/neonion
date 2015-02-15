0b76d0ceacf9a07f4d8711a1f0ced225555cd814
Saskia Lindner <saskia.li@fu-berlin.de>
2015-02-15 23:41:43 +0100

    Add AnnOccurCtrl to controller.js

--- neonion/static/js/controllers.js
+++ neonion/static/js/controllers.js
@@ -41,11 +41,11 @@ neonionApp.controller('AnnotationStoreCtrl', ['$scope', '$http', function ($scop
         var occurrences = {};
 
         data.forEach(function(a) {
+            var key = a.quote;
             var annotation = a.quote;
             // var type = a.typeof;
             var date = a.created;
             // erweitern wenn typeof nicht mehr zu Fehler führt
-            var key = a.quote;
             var doc = a.uri;
 
             if (!(key in occurrences)) {
@@ -58,13 +58,32 @@ neonionApp.controller('AnnotationStoreCtrl', ['$scope', '$http', function ($scop
                     occurrences[key].last = date;
                 }
             }
-            var dings = occurrences[key].docs;
 
             if (((occurrences[key].docs).indexOf(doc)) == -1) {
                 occurrences[key].docs.push(doc);
             }
         });
         $scope.occurrences = occurrences;
+        console.log(occurrences);
+    });
+}]);
+
+neonionApp.controller('AnnOccurCtrl', ['$scope', '$http', function ($scope, $http) {
+    "use strict";
+
+    $http.get('/api/store/annotations').success(function (data) {
+        var ann_occur = {};
+
+        data.forEach(function(a) {
+            // context variable here
+            var key = a.id;
+            var text = a.uri;
+            var date = a.created;
+
+            ann_occur[key] = {doc: text, created: date};
+        });
+        $scope.ann_occur = ann_occur;
+        console.log(ann_occur);
     });
 }]);
 
@@ -81,41 +100,6 @@ neonionApp.controller('AnnDocsCtrl', ['$scope', '$http', function ($scope, $http
             }
         });
         $scope.ann_docs = ann_docs;
-        console.log(ann_docs);
-    });
-}]);
-
-neonionApp.controller('AnnotationStoreCtrl', ['$scope', '$http', function ($scope, $http) {
-    "use strict";
-
-    $http.get('/api/store/annotations').success(function(data) {
-        $scope.annotations = data;
-        var occurrences = {};
-
-        data.forEach(function(a) {
-            var annotation = a.quote;
-            // var type = a.typeof;
-            var date = a.created;
-            var key = a.quote;
-            var doc = a.uri;
-
-            if (!(key in occurrences)) {
-                occurrences[key] = {ann: annotation, count:  1, last: date, docs: new Array()};
-            } else {
-                occurrences[key].count++;
-                var parsedDate = Date.parse(date);
-                var parsedOld = Date.parse(occurrences[key].last);
-                if (parsedDate > parsedOld) {
-                    occurrences[key].last = date;
-                }
-            }
-            var dings = occurrences[key].docs;
-
-            if (((occurrences[key].docs).indexOf(doc)) == -1) {
-                occurrences[key].docs.push(doc);
-            }
-        });
-        $scope.occurrences = occurrences;
     });
 }]);
 

a52052e267485a29387dd4c8b6768b9bfcf2fe7c
phobosultra <andre.breitenfeld@gmx.de>
2015-03-18 13:17:33 +0100

    updated NER model parameters

--- neonion/static/js/controllers.js
+++ neonion/static/js/controllers.js
@@ -585,22 +585,24 @@ neonionApp.controller('QueryCtrl', ['$scope', function ($scope) {
 
 }]);
 
+/**
+ * NER model controller
+ */
 neonionApp.controller('NamedEntityCtrl', ['$scope', function ($scope) {
     "use strict";
 
     $scope.models = [
         {
-            identifier: 'Standard',
-            stanfordModel: 'dewac_175m_600',
-            predictorNumberOfComponents: '?',
-            predictorNumberOfIterations: '(10^6) / n',
-            predictorLossFunction: 'log',
-            predictorAlpha: '?',
-            numberOfHiddenLayerComponents: 4,
-            predictorInitialize: 'Standard',
-            learnNetworkInitialize: 'Standard',
-            predictorLearn: false,
-            learnNetworkLearn: false
+            identifier: "Standard",
+            parameters: [
+                {label: "Name", type: "string", restriction: "must be unique", default: "Standard", updatable: false},
+                {label: "Stanford Model", type: "string", restriction: "available model in classifiers", default: "dewac_175m_600", updatable: true},
+                {label: "Learn-network: number of hidden layer components", type: "integer", restriction: "", default: 10, updatable: false},
+                {label: "Learn-Network: Initialize", type: "string", restriction: "Random or Standard", default: "Standard", updatable: false},
+                {label: "Predictor: Learn", type: "boolean", restriction: "", default: false, updatable: true},
+                {label: "Learn-Network: Learn", type: "boolean", restriction: "", default: false, updatable: true},
+                {label: "Predictor: Inverse of Regularization Strength", type: "float", restriction: "> 0", default: 1000, updatable: false}
+            ]
         }
     ];
 }]);

be70e3e438b3142749278be5e020b4c33789cbb9
Saskia Lindner <saskia.li@fu-berlin.de>
2015-03-30 12:05:49 +0200

    Add secondary meta-data properties

--- neonion/static/js/controllers.js
+++ neonion/static/js/controllers.js
@@ -608,7 +608,16 @@ neonionApp.controller('NamedEntityCtrl', ['$scope', function ($scope) {
 neonionApp.controller('MetaDataCtrl', ['$scope', '$http', function ($scope, $http) {
     "use strict";
 
-    $scope.metaDataElements = ['Contributor', 'Coverage', 'Creator', 'Date', 'Description',
+    var primaryMetaData = ['Title', 'Creator', 'Type'];
+
+    var secondaryMetaData = ['Contributor', 'Coverage', 'Date', 'Description',
                             'Format', 'Identifier', 'Language', 'Publisher', 'Relation',
-                            'Rights', 'Source', 'Subject', 'Title', 'Type']
+                            'Rights', 'Source', 'Subject'];
+
+    $scope.primaryMetaData = primaryMetaData;
+    $scope.secondaryMetaData = secondaryMetaData;
+
+    console.log($scope.primaryMetaData);
+    console.log($scope.secondaryMetaData);
+
 }]);

b8f34a33e0104c5877d50b08c55313672e8b4c31
Alexa Schlegel <alexa.schlegel@gmail.com>
2015-05-15 18:46:08 +0200

    NEO-419: Contibuterlist moved to the sidebar and is working
    * if nobody has annotated yet, the button is disabled
    * eye and crossed-eye is used now (because if everything is hidden no eye is displayed at all so the user would know what to do)
    * the color is not transparent anymore (otherwise the orange would mix with contributercolor)
    * contributers are moved to a service

--- neonion/static/js/services.js
+++ neonion/static/js/services.js
@@ -127,6 +127,8 @@ neonionApp.factory('AnnotatorService', ['$http', function($http) {
     var factory = {};
     var annotator;
 
+    factory.contributors = [];
+
     factory.annotator = function(value) {
         if (value) {
           annotator = value;
@@ -153,6 +155,50 @@ neonionApp.factory('AnnotatorService', ['$http', function($http) {
             // blink for more attention
             for (var i = 0; i < 2; i++) {
                 $(target).fadeTo('slow', 0.5).fadeTo('slow', 1.0);
+            }
+        }
+    };
+
+    factory.refreshContributors = function () {
+        var users = annotator.plugins.Neonion.getContributors();
+        var items = [];
+
+        users.forEach(function (user, index) {
+            var idx = factory.contributors.map(function (x) {
+                return x.user;
+            }).indexOf(user);
+            var showAnnotation = idx !== -1 ? factory.contributors[idx].showAnnotation : true;
+            var lastAnnotation = annotator.plugins.Neonion.getLastAnnotation(user);
+            var isoUpated = lastAnnotation.updated ? lastAnnotation.updated : new Date().toISOString();
+            items.push({
+                user: user, // creator of annotation
+                updated: isoUpated, // date when annotation was updated
+                showAnnotation: showAnnotation,
+                color: "hsla( " + factory.makeColor(index, users.length) + ", 50%, 75%, 1 )"
+            });
+        });
+
+        factory.contributors = items;
+    };
+
+    factory.makeColor = function (colorNum, colors) {
+        if (colors < 1) {
+            // defaults to one color - avoid divide by zero
+            colors = 1;
+        }
+        return ( colorNum * (360 / colors) ) % 360;
+    };
+
+    factory.colorizeAnnotation = function (annotation) {
+        if (annotation.creator) {
+            var idx = factory.contributors.map(function (x) {
+                return x.user;
+            }).indexOf(annotation.creator.email);
+            if (idx !== -1) {
+                var color = factory.contributors[idx].color;
+                annotation.highlights.forEach(function (highlight) {
+                    $(highlight).css("backgroundColor", color);
+                });
             }
         }
     };

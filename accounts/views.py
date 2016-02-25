03884af54232ea791da0632ba39892cb9fbbbb74
phobosultra <andre.breitenfeld@gmx.de>
2016-02-25 22:56:42 +0100

    Update user model * add and use "username" field in model to authenticate users * add template tag to check whether registration is possible or not * delete NeonionUserManager

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.contrib.auth import login as django_login, authenticate, logout as django_logout
@@ -11,7 +12,7 @@ def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            user = authenticate(email=request.POST['email'], password=request.POST['password'])
+            user = authenticate(username=request.POST['username'], password=request.POST['password'])
             if user is not None:
                 if user.is_active:
                     django_login(request, user)
@@ -28,20 +29,23 @@ def register(request):
     """
     User registration view.
     """
-    success = False
-    show_activation_info = False
-    if request.method == 'POST':
-        form = RegistrationForm(data=request.POST)
-        if form.is_valid():
-            user = form.save()
-            success = True
-            show_activation_info = not user.is_active
-    else:
-        form = RegistrationForm()
+    if 'accounts.backends.EmailAuthBackend' in settings.AUTHENTICATION_BACKENDS:
+        success = False
+        show_activation_info = False
+        if request.method == 'POST':
+            form = RegistrationForm(data=request.POST)
+            if form.is_valid():
+                user = form.save()
+                success = True
+                show_activation_info = not user.is_active
+        else:
+            form = RegistrationForm()
 
-    return render_to_response('register.html', {
-        'form': form, 'success': success, 'show_activation_info': show_activation_info
-    }, context_instance=RequestContext(request))
+        return render_to_response('register.html', {
+            'form': form, 'success': success, 'show_activation_info': show_activation_info
+        }, context_instance=RequestContext(request))
+    else:
+        return redirect('accounts.views.login')
 
 
 def logout(request):
